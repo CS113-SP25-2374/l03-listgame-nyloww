e45597b8f83233d83c8234ee5021c8aa8741ad55
katanu <katanu.mwendwa@gmail.com>
2025-03-06 14:07:11 -0800

    step 1 and step 2 done

--- src/main/java/cs113/listGame/enemies/EnemyArrayList.java
+++ src/main/java/cs113/listGame/enemies/EnemyArrayList.java
@@ -1,19 +1,49 @@
 package cs113.listGame.enemies;
 
 import cs113.listGame.behaviors.Behavior;
+import cs113.listGame.behaviors.FollowObject;
+import cs113.listGame.behaviors.MoveToPoint;
+import cs113.listGame.behaviors.Waiting;
 import cs113.listGame.gamecore.GameObject;
+import cs113.listGame.gamecore.PlayerObject;
+import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class EnemyArrayList extends EnemyObject {
-    public EnemyArrayList(ImageView imageView) { super(imageView); }
+    private final List<Behavior> behaviorList;
+    private final Random random;
+
+    public EnemyArrayList(ImageView imageView) {
+        super(imageView);
+        this.behaviorList = new ArrayList<>();
+        this.random = new Random();
+        seedBehaviors();
+    }
+
 
     @Override
     public void seedBehaviors() {
+        behaviorList.clear();
 
+        GameObject PlayerObject = null;
+        behaviorList.add(new FollowObject(this, 2000, null));
+        behaviorList.add(new MoveToPoint(this, 3000, new Point2D(100, 200)));
+        behaviorList.add(new Waiting(this, 2500));
+        behaviorList.add(new FollowObject(this, 1500, null));
+        behaviorList.add(new MoveToPoint(this, 2200, new  Point2D(400, 500)));
     }
 
     @Override
     public Behavior getNextBehavior() {
-        return null;
+        if (behaviorList.isEmpty()) {
+            seedBehaviors();
+        }
+
+        int index = random.nextInt(behaviorList.size());
+        return behaviorList.remove(index);
     }
 }
--- src/main/java/cs113/listGame/resources/ResourceArrayList.java
+++ src/main/java/cs113/listGame/resources/ResourceArrayList.java
@@ -2,24 +2,42 @@ package cs113.listGame.resources;
 
 import cs113.listGame.gamecore.GameObject;
 
+import java.util.ArrayList;
+
 public class ResourceArrayList implements ResourceList {
+
+    private final ArrayList<ResourceObject> resources;
+
+    public ResourceArrayList() {
+        resources = new ArrayList<>();
+    }
+
     @Override
     public void add(ResourceObject resource) {
-
+        resources.add(resource);
     }
 
     @Override
     public void remove(ResourceObject resource) {
-
+        resources.remove(resource);
     }
 
     @Override
     public void truncate(ResourceObject resource) {
-
+        int index = resources.indexOf(resource);
+        if (index != -1){
+            resources.subList(index, resources.size()).clear();
+        }
     }
 
     @Override
     public void follow(GameObject leader) {
+        if (resources.isEmpty()) return;
 
+        resources.getFirst().moveTowards(leader);
+
+        for (int i = 1; i < resources.size(); i++) {
+            resources.get(i).moveTowards(resources.get(i - 1));
+        }
     }
 }
